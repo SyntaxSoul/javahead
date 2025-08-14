e02fc825f0d8a83b1ebe685f94703e044c55dc72
SyntaxSoul <vinuofficial2021@gmail.com>
2025-08-15 01:32:14 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -26,31 +26,4 @@ public class LinkedList {
     public int tail(){
         return tail.value;
     }
-
-//    public void add(int value) {
-//        if(head==null && tail==null) {
-//            Node newNode=new Node(value);
-//            head=newNode;
-//            tail=newNode;
-//            length=1;
-//        }
-//        else {
-//            Node newNode=new Node(value);
-//            tail=newNode;
-//            next=newNode;
-//            length++;
-//        }
-//    }
-//    public int getFirst() {
-//        return head.value;
-//    }
-//    public int getLast() {
-//        return tail.value;
-//    }
-//    public int length() {
-//        return length;
-//    }
-
-
-
 }
